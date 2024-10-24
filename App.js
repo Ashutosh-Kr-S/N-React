3ededf5985cfdf1fe33e840f723305189e500ee2
N-React <cryocoders@gmail.com>
2024-10-24 21:29:07 +0530

    Added both packges and gitignore

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div',{id:"parent"},
-    React.createElement("div",{id:"child"}, 
-        [React.createElement('h1',{},'I am h1 tag !'), React.createElement('h2',{},'I am h2 tag !'), React.createElement('h3',{},'I am h3 tag !')]
-    )
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is a React project 🚀 !"),
+    React.createElement("h2", {}, "I am h2 tag !"),
+    React.createElement("h3", {}, "I am h3 tag !"),
+  ])
 );
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Recat projcet 🚀
+
+
+# Parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement (means refreshing when ans changes are made)
+- File Watching Algo - witten in C++
+- Cashing = Faster builds
+- Image optimization
+- Minification of file
+- Bundling the files
+- Compressing
+- Consistent Hashing =  is a technique used in distributed systems to store data and ensure that it's balanced and efficiently retrieved
+- Code splitting
+- Differential Bundling = so that it works smoothly on older browsers.
+- Diagnostics
+- Error Handlaing
+- Https = if we to work on ssl then we can
+- Tree shaking - remove unused code
+- Different dev and production bundles
