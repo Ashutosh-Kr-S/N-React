0e823c50c66fa4551ede24db09f599c0c643f8b5
N-React <cryocoders@gmail.com>
2024-12-19 18:55:26 +0530

    last

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,3 +1,5 @@
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfVGKp4Talhkb6oqs86YMfygI1JyMPv0TNoQ&s";
+export const LOGO_URL = "https://foodfire.onrender.com/api/restaurants?lat=12.9351929&lng=77.624480699999991&page_type=DESKTOP_WEB_LISTING";
+// export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfVGKp4Talhkb6oqs86YMfygI1JyMPv0TNoQ&s";
 export const CDN_URL =  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=";
+// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=";
+export const MENU_API = "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.624480699999991&&submitAction=ENTER&restaurantId=";
